904a940688a84fee75fa71f0f85a4aa240408e1c
martinbolle <70632679+martinbolle@users.noreply.github.com>
2021-10-18 09:27:38 +0200

    -Lagt til pre order, in order og post order. Brukt rekursive kall for utskrift. -Også laget en ikke-rekursiv metode som skriver ut pre-order

--- src/BinaryTree/BinaryTree.java
+++ src/BinaryTree/BinaryTree.java
@@ -49,6 +49,49 @@ public class BinaryTree {
         }
     }
 
+    static void printPreOrder(BinaryTreeNode node){
+        if(node == null){
+            return;
+        }
+        System.out.print(node.value + " ");
+        printPreOrder(node.left_child);
+        printPreOrder(node.right_child);
+    }
+
+    static void printInOrder(BinaryTreeNode node){
+        if(node == null){
+            return;
+        }
+        printInOrder(node.left_child);
+        System.out.print(node.value + " ");
+        printInOrder(node.right_child);
+    }
+
+    static void printPostOrder(BinaryTreeNode node){
+        if(node == null){
+            return;
+        }
+        printPostOrder(node.left_child);
+        printPostOrder(node.right_child);
+        System.out.print(node.value + " ");
+    }
+
+    static void printPreOrderNonRecursive(BinaryTreeNode root){
+        ArrayDeque<BinaryTreeNode> stack = new ArrayDeque<BinaryTreeNode>();
+        stack.addLast(root);
+
+        while(!stack.isEmpty()){
+            BinaryTreeNode current = stack.removeLast();
+            if(current.right_child != null) {
+                stack.addLast(current.right_child);
+            }
+            if(current.left_child != null) {
+                stack.addLast(current.left_child);
+            }
+            System.out.print(current.value + " ");
+        }
+    }
+
     public static void main(String[] args) {
         BinaryTreeNode root = new BinaryTreeNode('A');
 
@@ -64,6 +107,21 @@ public class BinaryTree {
         BinaryTreeNode g = c.addRightChild('G');
 
         //Kaller funksjonen vår.
-        printLevelOrder(root);
+        //printLevelOrder(root);
+
+        System.out.println("Pre order");
+        printPreOrder(root);
+        System.out.println();
+
+        System.out.println("In Order");
+        printInOrder(root);
+        System.out.println();
+
+        System.out.println("Post Order");
+        printPostOrder(root);
+
+        System.out.println();
+        System.out.println("Pre order non recursive");
+        printPreOrderNonRecursive(root);
     }
 }
